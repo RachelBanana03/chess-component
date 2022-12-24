ed45044134427d72acaec1d5998af79c7ecf2ac0
Rachel Banana <117894827+RachelBanana03@users.noreply.github.com>
2022-12-24 16:45:00 +0800

    Add chess pieces SVGs

---
+++ src/images/chess_pieces/index.js
@@ -0,0 +1,19 @@
+import p from "./pawn_black.svg";
+import n from "./knight_black.svg";
+import b from "./bishop_black.svg";
+import r from "./rook_black.svg";
+import q from "./queen_black.svg";
+import k from "./king_black.svg";
+
+import P from "./pawn_white.svg";
+import N from "./knight_white.svg";
+import B from "./bishop_white.svg";
+import R from "./rook_white.svg";
+import Q from "./queen_white.svg";
+import K from "./king_white.svg";
+
+const chess_pieces = {
+    p, n, b, q, r, k,
+    P, N, B, Q, R, K
+}
+export default chess_pieces;
