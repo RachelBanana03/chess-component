f32feaf27a7428a2dc2052d1ad6ed62a65008c17
Rachel Banana <117894827+RachelBanana03@users.noreply.github.com>
2022-12-25 13:13:08 +0800

    Add possibleMoves function

--- src/utils/Chess.js
+++ src/utils/Chess.js
@@ -1,7 +1,7 @@
 class Chess {
     #board;
     #moves;
-    #turn;
+    #isWhiteTurn;
     // 50-move rule
     // castling
     // en passant
@@ -15,38 +15,46 @@ class Chess {
             this.#board[6] = ["P", "P", "P", "P", "P", "P", "P", "P"];
             this.#board[7] = ["R", "N", "B", "Q", "K", "B", "N", "R"];
             this.#moves = [];
-            this.#turn = true;
+            this.#isWhiteTurn = true;
         }
     }
 
     static moveSets = {
         "n": {
-            moves: [[1, 2],[2, 1],[2, -1],[1, -2],[-1, -2],[-2, -1],[-2, 1],[-1, 2]],
+            directions: [[1, 2],[2, 1],[2, -1],[1, -2],[-1, -2],[-2, -1],[-2, 1],[-1, 2]],
             longRange: false
         },
         "b": {
-            moves: [[1, 1],[1, -1],[-1, -1],[-1, 1]],
+            directions: [[1, 1],[1, -1],[-1, -1],[-1, 1]],
             longRange: true
         },
         "r": {
-            moves: [[0, 1],[1, 0],[0, -1],[-1, 0]],
+            directions: [[0, 1],[1, 0],[0, -1],[-1, 0]],
             longRange: true
         },
         "q": {
-            moves: [
+            directions: [
                 [0, 1],[1, 0],[0, -1],[-1, 0],
                 [1, 1],[1, -1],[-1, -1],[-1, 1]
             ],
             longRange: true
         },
         "k": {
-            moves: [
+            directions: [
                 [0, 1],[1, 0],[0, -1],[-1, 0],
                 [1, 1],[1, -1],[-1, -1],[-1, 1]
             ],
             longRange: false
         }
     };
+
+    static inBoard(...positions) {
+        return [].concat(...positions).every(i=> i>=0 && i<8);
+    }
+
+    static isWhite(piece) {
+        return /[PNBRQK]/.test(piece);
+    }
 
     static isFEN(str) {
 
@@ -65,7 +73,7 @@ class Chess {
     }
 
     get turn() {
-        return this.#turn? "black": "white";
+        return this.#isWhiteTurn? "white": "black";
     }
 
     move(start, end) {
@@ -77,29 +85,50 @@ class Chess {
         this.#board[start[0]][start[1]] = null;
         this.#board[end[0]][end[1]] = piece;
         this.#moves.push([start, end]);
-        this.#turn = !this.#turn;
+        this.#isWhiteTurn = !this.#isWhiteTurn;
         return true;
     }
 
-    #possibleMoves(piece, start, end) {
+    #possibleMoves(piece, start) {
         // if pawn, different rules
-        // also castling?
+        if (piece.toLowerCase()==="p") {
+            // en passant?
+        }
 
-        //
-        const {moves, longRange} = Chess.moveSets[piece.toLowerCase()];
+        // castling? king checks?
+        const moves = [];
+        const {directions, longRange} = Chess.moveSets[piece.toLowerCase()];
+        for (const dir of directions) {
+            let curPos = start;
+            do {
+                curPos = [curPos[0] + dir[0], curPos[1] + dir[1]];
+                // if position is not in board
+                if (!Chess.inBoard(curPos)) break;
+
+                // if position has piece of same color
+                const opponent = this.#board[curPos[0]][curPos[1]];
+                if (opponent && Chess.isWhite(opponent) === Chess.isWhite(piece)) break;
+
+                moves.push(curPos);
+
+                // if position already has a blockade
+                if (opponent) break;
+            } while (longRange)
+        }
+        return moves;
     }
 
     isLegal(start, end) {
         // check if positions are in board
-        if ([...start, ...end].some(i=> i<0 || i>7)) return false;
+        if (!Chess.inBoard(start, end)) return false;
         // if start and end are the same
         if (start[0]===end[0] && start[1]===end[1]) return false;
         const piece = this.#board[start[0]][start[1]];
         // if there is no piece or if piece does not belong to player
-        if (!piece || /[PNBRQK]/.test(piece) !== this.#turn) return false;
+        if (!piece || Chess.isWhite(piece) !== this.#isWhiteTurn) return false;
         // if end is own piece
         const opponent = this.#board[end[0]][end[1]];
-        if (opponent && /[PNBRQK]/.test(opponent) === this.#turn) return false;
+        if (opponent && Chess.isWhite(opponent) === this.#isWhiteTurn) return false;
         
         // check if piece moveset can move to end legally
         // check if path is blocked
